4e28af3d75e5dd4cfa79a040ad9231f492e702ec
PsG <psilvagontijo@gmail.com>
2025-04-13 14:48:22 -0300

    Refactoring: Configuração inicial

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: { bodySizeLimit: "1mb", allowedOrigins: ["*"] }, 
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+}
 
 export default nextConfig;
